c96d841bf7a3c950da6346459cb1312dcc5b8b5f
Forrest McDougal <forrestmcdougal@gmail.com>
2019-03-24 15:33:25 -0400

    initial commit

---
+++ logic.js
@@ -0,0 +1,90 @@
+// Store our API endpoint inside queryUrl
+const queryUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
+
+const streetmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution:
+		'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: API_KEY
+});
+
+const darkmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution:
+		'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.dark',
+	accessToken: API_KEY
+});
+
+const baseMaps = {
+	'Street Map': streetmap,
+	'Dark Map': darkmap
+};
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+	console.log(data);
+	let earthquakeMarkers = [];
+
+	for (let i = 0; i < data.features.length; i++) {
+		let color = '';
+		let mag = data.features[i].properties.mag;
+		if (mag >= 5) {
+			color = '#FF0000';
+		} else if (mag >= 4) {
+			color = '#FF6900';
+		} else if (mag >= 3) {
+			color = '#FFD300';
+		} else if (mag >= 2) {
+			color = '#C2FF00';
+		} else if (mag >= 1) {
+			color = '#58FF00';
+		} else {
+			color = '#00FF00';
+		}
+		earthquakeMarkers.push(
+			L.circle([ data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0] ], {
+				fillOpacity: 0.75,
+				color: color,
+				fillColor: color,
+				// Adjust radius
+				radius: mag * 15000
+			}).bindPopup(`<h1>${data.features[i].properties.place}</h1>
+         <hr>
+         <h3>Date: ${new Date(data.features[i].properties.time)}</h3>
+         <h3>Magnitude: ${mag}</h3>`)
+		);
+	}
+	let earthquakeLayer = L.layerGroup(earthquakeMarkers);
+
+	var overlayMaps = {
+		Earthquakes: earthquakeLayer
+	};
+
+	var myMap = L.map('map', {
+		center: [ 37.09, -95.71 ],
+		zoom: 6,
+		layers: [ streetmap, earthquakeLayer ]
+	});
+	L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+	let legend = L.control({ position: 'bottomright' });
+
+	legend.onAdd = function(map) {
+		var div = L.DomUtil.create('div', 'info legend');
+		div.innerHTML += 'Magnitude<br><hr>';
+		let colors = [ '#00FF00', '#58FF00', '#C2FF00', '#FFD300', '#FF6900', '#FF0000' ];
+		let grades = [ '0-1', '1-2', '2-3', '3-4', '4-5', '5+' ];
+
+		// loop through our density intervals and generate a label with a colored square for each interval
+		for (var i = 0; i < 6; i++) {
+			div.innerHTML +=
+				'<div><i style="background:' + colors[i] + '">&nbsp&nbsp&nbsp&nbsp</i> ' + grades[i] + '</div>';
+		}
+
+		return div;
+	};
+
+	legend.addTo(myMap);
+});
